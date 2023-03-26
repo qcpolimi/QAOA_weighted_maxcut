e621e0b2fb0751a6fe2460503903fbbc11dc1d09
quosta <costantino.carugno@polimi.it>
2023-03-26 15:08:59 +0200

    final version

--- plot.py
+++ plot.py
@@ -1,72 +1,302 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 14 14:55:45 2023
-
-@author: rugantio
-"""
-
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# x = range(4,21,2)
-# y_perimeter = (0.80, 0.82, 0.86, 0.911, 0.945, 0.908, 0.973, 0.941, 0.927)
-# y_star = (1, 1, 1, 1, 1, 1, 1, 1, 1)
-# y_star_xy = (1, 0.84, 0.8, 0.678, 0.59,0.64, 
-#              0.667, 0.629, 0.65)   
+folder = '/home/rugantio/Downloads/QAOA_weighted_maxcut/exp_cobyla_final/'
 
-# fig = plt.figure(3, figsize=(10.24, 7.68))
-# # title = optimizer + ', n = ' + str(n) + ', depth = ' + str(depth) \
-# #     + ', mixer = ' + mixer + ', mu = ' + str(np.round(mu, 3)) + \
-# #     ', rnd = ' + str(np.round(rnd_mu, 3))
-# # plt.title(title)
-# plt.plot(x, y_star, linestyle='None', marker='o', color='black', label=None)
-# plt.plot(x, y_star, marker='o', color='black', label='x and r optimizer')
-# plt.plot(x, y_star_xy, linestyle='None', marker='o', color='black', label=None)
-# plt.plot(x, y_star_xy, marker='o', color='black', label='xy optimizer')
-# plt.xlabel("Qubits")
-# plt.ylabel("Approx ratio")
-
-
-# #plt.axhline(0.5, linestyle='-', color='black', label='average')
-# #plt.axhline(1, linestyle='--', color='black', label='max')
-# plt.xticks(range(2,21,2))#np.arange(0, experiments+1, step=5))
-
-# plt.legend()# plt.yticks(np.arange(0, 1.2, step=0.2))
-# plt.grid()
-# # plt.plot(x,y)
-# fig.savefig("/home/rugantio/Downloads/approx2.svg", bbox_inches='tight', pad_inches=0)
-# plt.show()
-# plt.clf()
+p_x_p1 = pd.read_csv(folder + "perimeter_COBYLA_x_p=1.csv", index_col='n_qubs')
+p_r_p1 = pd.read_csv(folder + "perimeter_COBYLA_r_p=1.csv", index_col='n_qubs')
+p_xy_p1 = pd.read_csv(folder + "perimeter_COBYLA_xy_p=1.csv", index_col='n_qubs')
+p_x_p2 = pd.read_csv(folder + "perimeter_COBYLA_x_p=2.csv", index_col='n_qubs')
+p_r_p2 = pd.read_csv(folder + "perimeter_COBYLA_r_p=2.csv", index_col='n_qubs')
+p_xy_p2 = pd.read_csv(folder + "perimeter_COBYLA_xy_p=2.csv", index_col='n_qubs')
+p_x_p3 = pd.read_csv(folder + "perimeter_COBYLA_x_p=3.csv", index_col='n_qubs')
+p_r_p3 = pd.read_csv(folder + "perimeter_COBYLA_r_p=3.csv", index_col='n_qubs')
+p_xy_p3 = pd.read_csv(folder + "perimeter_COBYLA_xy_p=3.csv", index_col='n_qubs')
+p_x_p4 = pd.read_csv(folder + "perimeter_COBYLA_x_p=4.csv", index_col='n_qubs')
+p_r_p4 = pd.read_csv(folder + "perimeter_COBYLA_r_p=4.csv", index_col='n_qubs')
+p_xy_p4 = pd.read_csv(folder + "perimeter_COBYLA_xy_p=4.csv", index_col='n_qubs')
+s_x_p1 = pd.read_csv(folder + "star_COBYLA_x_p=1.csv", index_col='n_qubs')
+s_r_p1 = pd.read_csv(folder + "star_COBYLA_r_p=1.csv", index_col='n_qubs')
+s_xy_p1 = pd.read_csv(folder + "star_COBYLA_xy_p=1.csv", index_col='n_qubs')
+s_x_p2 = pd.read_csv(folder + "star_COBYLA_x_p=2.csv", index_col='n_qubs')
+s_r_p2 = pd.read_csv(folder + "star_COBYLA_r_p=2.csv", index_col='n_qubs')
+s_xy_p2 = pd.read_csv(folder + "star_COBYLA_xy_p=2.csv", index_col='n_qubs')
+s_x_p3 = pd.read_csv(folder + "star_COBYLA_x_p=3.csv", index_col='n_qubs')
+s_r_p3 = pd.read_csv(folder + "star_COBYLA_r_p=3.csv", index_col='n_qubs')
+s_xy_p3 = pd.read_csv(folder + "star_COBYLA_xy_p=3.csv", index_col='n_qubs')
+s_x_p4 = pd.read_csv(folder + "star_COBYLA_x_p=4.csv", index_col='n_qubs')
+s_r_p4 = pd.read_csv(folder + "star_COBYLA_r_p=4.csv", index_col='n_qubs')
+s_xy_p4 = pd.read_csv(folder + "star_COBYLA_xy_p=4.csv", index_col='n_qubs')
 
 
-x = range(4,21,2)
-y_perimeter = (0.80, 0.82, 0.86, 0.911, 0.945, 0.908, 0.973, 0.941, 0.927)
-y_perimeter2 = (1, 0.96, 0.943, 0.933, 0.964, 0.954, 0.987 , 1, 1)
-y_star = (1, 1, 1, 1, 1, 1, 1, 1, 1) 
-
+# perimeter p 1 all mixers
 fig = plt.figure(3, figsize=(10.24, 7.68))
-# title = optimizer + ', n = ' + str(n) + ', depth = ' + str(depth) \
-#     + ', mixer = ' + mixer + ', mu = ' + str(np.round(mu, 3)) + \
-#     ', rnd = ' + str(np.round(rnd_mu, 3))
-# plt.title(title)
-plt.plot(x, y_perimeter, linestyle='None', marker='x', color='#3371ff', label=None)
-plt.plot(x, y_perimeter, marker='o', color='#3371ff', label='cycle p=1')
-plt.plot(x, y_perimeter2, linestyle='None', marker='x', color='#a3bfff', label=None)
-plt.plot(x, y_perimeter2, marker='o', color='#a3bfff', label='cycle p=2')
-plt.plot(x, y_star, linestyle='None', marker='o', color='#ff3636', label=None)
-plt.plot(x, y_star, marker='o', color='#ff3636', label='star')
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.errorbar(x_1, p_x_p1.approx_ratio_mean, yerr= p_x_p1.approx_ratio_std, marker='s', color='#9200ff', label='cycle x')
+plt.errorbar(x_1, p_r_p1.approx_ratio_mean,  yerr= p_r_p1.approx_ratio_std, marker='o', color='#0000ff', label='cycle r')
+plt.errorbar(x_1, p_xy_p1.approx_ratio_mean,  yerr= p_xy_p1.approx_ratio_std,marker='*', color='#00ccff', label = 'cycle xy')
+# plt.axhline(0.5, linestyle='--', color='black', label='average')
+#plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+#plt.grid()
+#plt.legend()
+#plt.xlabel("Qubits")
+#plt.ylabel("Approx ratio")
+
+# star p 1 all mixers
+#fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+#plt.errorbar(x_1, s_x_p1.approx_ratio_mean, marker='s', color='#ff0095', label='s x')
+plt.errorbar(x_1, s_r_p1.approx_ratio_mean,  yerr= s_r_p1.approx_ratio_std, marker='o', color='#ff0000', label='star r and x')
+plt.errorbar(x_1, s_xy_p1.approx_ratio_mean,  yerr= s_xy_p1.approx_ratio_std,marker='*', color='#ff9100', label = 'star xy')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+fig.savefig("/home/rugantio/Downloads/approx.svg", bbox_inches='tight', pad_inches=0)
+
+
+
+# perimeter p 1 all mixers
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.plot(x_n, p_x_p2.approx_ratio_mean, marker='s', color='#9200ff', label='cycle x')
+plt.plot(x_n, p_r_p2.approx_ratio_mean, marker='o', color='#0000ff', label='cycle r')
+plt.plot(x_n, p_xy_p2.approx_ratio_mean, marker='*', color='#00ccff', label = 'cycle xy')
+#plt.axhline(0.5, linestyle='--', color='black', label='average')
+# plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+#plt.grid()
+#plt.legend()
+#plt.xlabel("Qubits")
+#plt.ylabel("Approx ratio")
+
+# star p 1 all mixers
+#fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+#plt.plot(x_n, s_x_p2.approx_ratio_mean, marker='s', color='#ff0095', label='s x')
+plt.plot(x_n, s_r_p2.approx_ratio_mean, marker='o', color='#ff0000', label='star x and r')
+plt.plot(x_n, s_xy_p2.approx_ratio_mean, marker='*', color='#ff9100', label = 'star xy')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
 plt.xlabel("Qubits")
 plt.ylabel("Approx ratio")
 
 
-#plt.axhline(0.5, linestyle='-', color='black', label='average')
-#plt.axhline(1, linestyle='--', color='black', label='max')
-plt.xticks(range(2,21,2))#np.arange(0, experiments+1, step=5))
-
-plt.legend()# plt.yticks(np.arange(0, 1.2, step=0.2))
+# perimeter x, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, p_x_p1.approx_ratio_mean[:9], marker='o', color='#ff0095', label='cycle p=1')
+plt.plot(x_n, p_x_p2.approx_ratio_mean, marker='o', color='#0D00C3', label='cycle p=2')
+plt.plot(x_n, p_x_p3.approx_ratio_mean, marker='o', color='#00C30D', label = 'cycle p=3')
+plt.plot(x_n, p_x_p4.approx_ratio_mean, marker='o', color='#9200ff', label='cycle p=4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
 plt.grid()
-# plt.plot(x,y)
-fig.savefig("/home/rugantio/Downloads/approx.svg", bbox_inches='tight', pad_inches=0)
-plt.show()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+fig.savefig("/home/rugantio/Downloads/slice_cycle.svg", bbox_inches='tight', pad_inches=0)
+
+
+# star x, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, s_x_p1.approx_ratio_mean[:9], marker='o', color='#ff0095', label='s 1')
+plt.plot(x_n, s_x_p2.approx_ratio_mean, marker='o', color='#ff0000', label='s 2')
+plt.plot(x_n, s_x_p3.approx_ratio_mean, marker='o', color='#ff9100', label = 's 3')
+plt.plot(x_n, s_x_p4.approx_ratio_mean, marker='o', color='#9200ff', label='s 4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# =============================================================================
+# # perimeter r, slice along p
+# fig = plt.figure(3, figsize=(10.24, 7.68))
+# x_n = p_x_p2.index
+# plt.plot(x_n, p_r_p1.approx_ratio_mean[:9], marker='o', color='#ff0095', label='p 1')
+# plt.plot(x_n, p_r_p2.approx_ratio_mean, marker='o', color='#ff0000', label='p 2')
+# plt.plot(x_n, p_r_p3.approx_ratio_mean, marker='o', color='#ff9100', label = 'p 3')
+# plt.plot(x_n, p_r_p4.approx_ratio_mean, marker='o', color='#9200ff', label='p 4')
+# plt.axhline(0.5, linestyle='--', color='black', label='average')
+# plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+# plt.grid()
+# plt.legend()
+# plt.xlabel("Qubits")
+# plt.ylabel("Approx ratio") 
+# =============================================================================
+
+
+# perimeter xy, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, p_xy_p1.approx_ratio_mean[:9], marker='o', color='#ff0095', label='p 1')
+plt.plot(x_n, p_xy_p2.approx_ratio_mean, marker='o', color='#ff0000', label='p 2')
+plt.plot(x_n, p_xy_p3.approx_ratio_mean, marker='o', color='#ff9100', label = 'p 3')
+plt.plot(x_n, p_xy_p4.approx_ratio_mean, marker='o', color='#9200ff', label='p 4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# star xy, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, s_xy_p1.approx_ratio_mean[:9], marker='o', color='#ff0095', label='star p=1')
+plt.plot(x_n, s_xy_p2.approx_ratio_mean, marker='o', color='#0D00C3', label='star p=2')
+plt.plot(x_n, s_xy_p3.approx_ratio_mean, marker='o', color='#00C30D', label = 'star p=3')
+plt.plot(x_n, s_xy_p4.approx_ratio_mean, marker='o', color='#9200ff', label='star p=4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+fig.savefig("/home/rugantio/Downloads/slice_star.svg", bbox_inches='tight', pad_inches=0)
+
+
+# =============================================================================
+# # optimal sol count
+# # perimeter x, slice along p
+# fig = plt.figure(3, figsize=(10.24, 7.68))
+# x_n = p_x_p2.index
+# plt.plot(x_n, p_x_p1.optimal_sol_count[:9], marker='o', color='#ff0095', label='p 1')
+# plt.plot(x_n, p_x_p2.optimal_sol_count, marker='o', color='#ff0000', label='p 2')
+# plt.plot(x_n, p_x_p3.optimal_sol_count, marker='o', color='#ff9100', label = 'p 3')
+# plt.plot(x_n, p_x_p4.optimal_sol_count, marker='o', color='#9200ff', label='p 4')
+# plt.axhline(0.5, linestyle='--', color='black', label='average')
+# plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+# plt.grid()
+# plt.legend()
+# plt.xlabel("Qubits")
+# plt.ylabel("Optimal solution count")
+# 
+# =============================================================================
+
+# OPTIMIZATION TIME
+# perimeter p 1 all mixers
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.semilogy(x_1, p_x_p1.opt_time_mean, marker='s', color='#9200ff', label='cycle x')
+plt.semilogy(x_1, p_r_p1.opt_time_mean, marker='o', color='#0000ff', label='cycle r')
+plt.semilogy(x_1, p_xy_p1.opt_time_mean, marker='*', color='#00ccff', label = 'cycle xy')
+# plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+# plt.grid()
+# plt.legend()
+# plt.xlabel("Qubits")
+# plt.ylabel("Optimization time")
+
+# star p 1 all mixers
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.semilogy(x_1, s_x_p1.opt_time_mean, marker='s', color='#ff0095', label='star x')
+plt.semilogy(x_1, s_r_p1.opt_time_mean, marker='o', color='#ff0000', label='star r')
+plt.semilogy(x_1, s_xy_p1.opt_time_mean, marker='*', color='#ff9100', label = 'star xy')
+plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Optimization time")
+
+fig.savefig("/home/rugantio/Downloads/opt_time.svg", bbox_inches='tight', pad_inches=0)
+
+
+# perimeter r, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.semilogy(x_n, p_r_p1.opt_time_mean[:9], marker='o', color='#ff0095', label='p 1')
+plt.semilogy(x_n, p_r_p2.opt_time_mean, marker='o', color='#ff0000', label='p 2')
+plt.semilogy(x_n, p_r_p3.opt_time_mean, marker='o', color='#ff9100', label = 'p 3')
+plt.semilogy(x_n, p_r_p4.opt_time_mean, marker='o', color='#9200ff', label='p 4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Optimization time")
+
+# star r, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.semilogy(x_n, s_r_p1.opt_time_mean[:9], marker='o', color='#ff0095', label='s 1')
+plt.semilogy(x_n, s_r_p2.opt_time_mean, marker='o', color='#ff0000', label='s 2')
+plt.semilogy(x_n, s_r_p3.opt_time_mean, marker='o', color='#ff9100', label = 's 3')
+plt.semilogy(x_n, s_r_p4.opt_time_mean, marker='o', color='#9200ff', label='s 4')
+# plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Optimization time")
+
+# OPT ITERATIONS 
+# perimeter xy, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, p_xy_p1.opt_iterations_mean[:9], marker='o', color='#ff0095', label='p 1')
+plt.plot(x_n, p_xy_p2.opt_iterations_mean, marker='o', color='#ff0000', label='p 2')
+plt.plot(x_n, p_xy_p3.opt_iterations_mean, marker='o', color='#ff9100', label = 'p 3')
+plt.plot(x_n, p_xy_p4.opt_iterations_mean, marker='o', color='#9200ff', label='p 4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# star xy, slice along p
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_n = p_x_p2.index
+plt.plot(x_n, s_xy_p1.opt_iterations_mean[:9], marker='o', color='#ff0095', label='s 1')
+plt.plot(x_n, s_xy_p2.opt_iterations_mean, marker='o', color='#ff0000', label='s 2')
+plt.plot(x_n, s_xy_p3.opt_iterations_mean, marker='o', color='#ff9100', label = 's 3')
+plt.plot(x_n, s_xy_p4.opt_iterations_mean, marker='o', color='#9200ff', label='s 4')
+plt.axhline(0.5, linestyle='--', color='black', label='average')
+plt.xticks(x_n)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# perimeter p 1 all mixers
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.plot(x_1, p_x_p1.opt_iterations_mean, marker='s', color='#9200ff', label='p x')
+plt.plot(x_1, p_r_p1.opt_iterations_mean, marker='o', color='#0000ff', label='p r')
+plt.plot(x_1, p_xy_p1.opt_iterations_mean, marker='*', color='#00ccff', label = 'p xy')
+plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# star p 1 all mixers
+fig = plt.figure(3, figsize=(10.24, 7.68))
+x_1 = p_x_p1.index
+x_n = p_x_p2.index
+plt.plot(x_1, s_x_p1.opt_iterations_mean, marker='s', color='#ff0095', label='s x')
+plt.plot(x_1, s_r_p1.opt_iterations_mean, marker='o', color='#ff0000', label='s r')
+plt.plot(x_1, s_xy_p1.opt_iterations_mean, marker='*', color='#ff9100', label = 's xy')
+plt.xticks(x_1)#np.arange(0, experiments+1, step=5))
+plt.grid()
+plt.legend()
+plt.xlabel("Qubits")
+plt.ylabel("Approx ratio")
+
+# # fig.savefig("/home/rugantio/Downloads/approx.svg", bbox_inches='tight', pad_inches=0)
 # plt.clf()
